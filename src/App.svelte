b449e6e32e08eb8cee6c8c10fc3d8178ab4e24f9
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2022-02-27 12:51:18 -0600

    🏗️ Scaffolding

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,22 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import Card from "./lib/Card.svelte";
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <Card>I have sentience, please don't kill the runtime!</Card>
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
+   main {
+     text-align: center;
+     padding: 0.5em;
+     max-width: 848px;
+     margin: 0 auto;
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
+     /* Perfect centering */
+     display: flex;
+     justify-content: center;
+     height: 100vh;
+     align-items: center;
+   }
 </style>
---
+++ src/lib/Card.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { fly } from 'svelte/transition';
+
+    export let definition = "definition";
+</script>
+
+<div id="card" in:fly={{ x: 200, duration: 500, delay: 500 }} out:fly={{ x: -200, duration: 500 }}>
+    <slot></slot>
+</div>
+
+<style>
+    #card {
+        position: relative;
+        overflow: auto;
+        max-height: calc(100vh - 1em);
+        background-color: white;
+        border-radius: 8px;
+        box-shadow: 0 .25rem 1rem 0 rgba(48, 53, 69, 0.08);
+        width: 100%;
+        height: fit-content;
+        padding: 0.5em;
+    }
+
+    #card :global(img) {
+        max-height: 50vh;
+        max-width: 100%;
+    }
+</style>
