cd8817c3d788e1de6c7989bac6f808cf06e8cffd
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2022-03-05 10:38:05 -0600

    🖥️ Server

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import fs from "fs/promises";
+const app = express();
+
+// Serve the static files from the Svelte app
+app.use(express.static('dist'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/scores", async (req, res) => {
+    res.json(JSON.parse(await fs.readFile("scores.json", "utf-8")));
+});
+
+// query params: user, score
+app.get("/scores/add", async (req, res) => {
+    console.log(req.query.user, req.query.score);
+    const scores = JSON.parse(await fs.readFile("scores.json", "utf-8"));
+    scores[req.query.user] = Number.parseInt(req.query.score) || 0;
+    res.json(scores);
+    fs.writeFile("scores.json", JSON.stringify(scores), "utf-8");
+});
+
+// Port CAN NOT be 3000 because that's already in use by Svelte dev server
+const port = process.env.PORT || 3030;
+app.listen(port);
+
+console.log("App is listening on port " + port);
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  server: {
+    proxy: {
+      // string shorthand, see [docs](https://vitejs.dev/config/#server-proxy)
+      '/scores': 'http://localhost:3030',
+    }
+  }
 })
