4312c2d3c4807164b4c47d1f923df4e24c7e1527
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2022-09-19 18:50:53 -0500

    💨 PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
+import manifest from './src/manifest.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    VitePWA({
+      injectRegister: 'inline',
+      manifest,
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
+        cleanupOutdatedCaches: true,
+      }
+    }),
+  ],
   base: 'PitchRead',
-  server: {
-    proxy: {
-      // string shorthand, see [docs](https://vitejs.dev/config/#server-proxy)
-      '/scores': 'http://localhost:3030',
-    }
-  }
 })
